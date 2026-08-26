2e45a4009264eac96cdad4c116fbc5bd570b0dd6
Md Farhan <faranmohd27@gmail.com>
2026-08-26 22:26:16 +0530

    Add Age.java to display user age

---
+++ Age.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class main{
+  public static void main(String[]args){
+   Scanner sc = new Scanner(System.in);
+  System.out.println("enter your age");
+  int a = sc.nextInt();
+System.out.println("your age is :"+a);
+}
+}
+
